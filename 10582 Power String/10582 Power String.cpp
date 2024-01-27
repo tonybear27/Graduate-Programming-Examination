6b87734bb9b8bc5539bfef1245ba3c554416b73b
Tony <128001273+tonybear27@users.noreply.github.com>
2024-01-27 21:02:20 +0800

    Update 10582 Power String.cpp

--- 10582 Power String/10582 Power String.cpp
+++ 10582 Power String/10582 Power String.cpp
@@ -4,35 +4,28 @@ using namespace std;
 int main() 
 {
     string s;
-    while (cin>>s && s!=".")
-	{
+    while (cin>>s && s!=".") {
         int ans=1;
         int N=s.size();
         
-		for (int i=N; i>0; i--)
-		{
+	for (int i=N; i>0; i--) {
             bool flag=true;
-            
-            if (!(N%i))
-			{
+            if (!(N%i)) {
                 string target = s.substr(0, i);
-				for (int j=i; j<N; j+=i)
-				{
-                    if (s.substr(j, i) != target)
-					{
+		for (int j=i; j<N; j+=i) {
+                    if (s.substr(j, i) != target) {
                         flag=false;
                         break;
                     }
                 }
                     
-				if(flag) 
-					ans = i;
+		if(flag) ans = i;
             }
         }
         
         cout << N/ans << "\n";
     
-	}
+     }
     
-	return 0;
+     return 0;
 }
